50f1cd566d14133772c0d3b1b342b31f5b0c84f8
Yousuf Zulqernine <c251060@ugrad.iiuc.ac.bd>
2026-06-21 23:07:23 +0600

    Test 2 done

---
+++ test_2.py
@@ -0,0 +1,10 @@
+print("\nLets get the 8s from +, -, *, /")
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
+
+print("\nRepresenting favourite number using variables")
+PI = 3.14
+number = 8*PI
+print(f"My favourite number is {number}")
