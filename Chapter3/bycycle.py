7095b4d2095a461d21e47315742ffa9faff2b7ec
Yousuf Zulqernine <c251060@ugrad.iiuc.ac.bd>
2026-06-24 21:35:06 +0600

    Some list practise with basics

--- Chapter3/bycycle.py
+++ Chapter3/bycycle.py
@@ -13,3 +13,22 @@ print(bicycles)
 bicycles = ['veloce', 'giant', 'phoenix', 'turbo']
 message = f"\nMy first bicycle was a {bicycles[0].title()}."
 print(message)
+
+#some practice with lists
+friends = ['Alice', 'Bob', 'Charlie', 'David']
+
+print(f"\n{friends[0]}")        # accessing the first element
+print(friends[1])               # accessing the second element
+print(friends[2])               # accessing the third element
+print(friends[3])               # accessing the fourth element
+
+print (f"\nMy friend {friends[0].title()} is a greedy person.")
+print (f"My friend {friends[1].title()} is a kind person.")
+print (f"My friend {friends[2].title()} is a helpful person.")
+print (f"My friend {friends[3].title()} is a friendly person.")
+
+motors = ['honda', 'yamaha', 'suzuki', 'kawasaki']
+print(f"\nI have a dream to get a ride on a {motors[0].title()} motorcycle.")
+print(f"I have a dream to get a ride on a {motors[1].title()} motorcycle.")
+print(f"I have a dream to get a ride on a {motors[2].title()} motorcycle.")
+print(f"I have a dream to get a ride on a {motors[3].title()} motorcycle.")
