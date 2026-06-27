069737b137b42bcefe08ab5eabf72fe24479fb95
Yousuf Zulqernine <c251060@ugrad.iiuc.ac.bd>
2026-06-27 22:10:46 +0600

    for loop  basic done

--- Chapter 4/for loop.py
+++ Chapter 4/for loop.py
@@ -1,3 +1,24 @@
+#normal for loop
 magicians = ['Alice', 'Bobby', 'Daeron', 'Halaand']
 for x in magicians:
     print(x)
+
+#printing in the loop
+magicians = ['alice', 'david', 'carolina']
+print("\n")
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print(f"Thank You guyzz , it was fun!!")
+
+#some try
+pizzas = ['Pepparoni', 'BBQ', 'Cocktail']
+print("\n")
+for shera in pizzas :
+    print(shera)
+pizzas = ['Pepparoni', 'BBQ', 'Cocktail']
+print("\n")
+for shera in pizzas :
+    print(f"WOW!!, the {shera.title()} was so tasty....")
+print(f"\nWish the people of palestine had these tasty pizzas!!!")    
+    
