b95298d78a5eae002720e1faaa29edcf632270b9
Yousuf Zulqernine <c251060@ugrad.iiuc.ac.bd>
2026-06-29 23:00:41 +0600

    List dividing

---
+++ Chapter 4/slicing list.py
@@ -0,0 +1,15 @@
+# Slicing a list by ratio
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[1:4])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[:4])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[2:])
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
